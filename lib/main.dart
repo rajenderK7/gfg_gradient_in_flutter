bfec253f78b506d7298607202117c511a71c59e6
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2021-12-11 23:41:48 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -43,84 +43,141 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              // For Linear Gradient without stops and tileMode
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: Container(
-              //     height: MediaQuery.of(context).size.height * 0.7,
-              //     width: MediaQuery.of(context).size.width * 0.9,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(20),
-              //       gradient: const LinearGradient(
-              //         colors: [Colors.purple, Colors.blueAccent],
-              //         begin: Alignment.bottomLeft,
-              //         end: Alignment.topRight,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              // For Linear Gradient with stops and tileMode
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: Container(
-              //     height: MediaQuery.of(context).size.height * 0.7,
-              //     width: MediaQuery.of(context).size.width * 0.9,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(20),
-              //       gradient: const LinearGradient(
-              //         colors: [Colors.purple, Colors.blueAccent],
-              //         begin: Alignment.bottomLeft,
-              //         end: Alignment.topRight,
-              //         stops: [0.4, 0.7],
-              //         tileMode: TileMode.repeated,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              // For Radial Gradient without stops and tileMode
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: Container(
-              //     height: MediaQuery.of(context).size.height * 0.7,
-              //     width: MediaQuery.of(context).size.width * 0.9,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(20),
-              //       gradient: const RadialGradient(
-              //         colors: [Colors.red, Colors.yellow],
-              //         radius: 0.75,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              // For Radial Gradient with stops and tileMode
-              // Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              //   child: Container(
-              //     height: MediaQuery.of(context).size.height * 0.7,
-              //     width: MediaQuery.of(context).size.width * 0.9,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(20),
-              //       gradient: const RadialGradient(
-              //         colors: [Colors.red, Colors.yellow],
-              //         radius: 0.75,
-              //         focal: Alignment(0.7, -0.7),
-              //         tileMode: TileMode.clamp,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Radial Gradient",
-                style: GoogleFonts.lato(
-                  fontSize: 30,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // For Linear Gradient without stops and tileMode
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Column(
+                //     children: [
+                //       Container(
+                //         height: MediaQuery.of(context).size.height * 0.7,
+                //         width: MediaQuery.of(context).size.width * 0.9,
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.circular(20),
+                //           gradient: const LinearGradient(
+                //             colors: [Colors.purple, Colors.blueAccent],
+                //             begin: Alignment.bottomLeft,
+                //             end: Alignment.topRight,
+                //           ),
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //       Text(
+                //         "Linear Gradient",
+                //         style: GoogleFonts.lato(
+                //           fontSize: 30,
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                // For Linear Gradient with stops and tileMode
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Column(
+                //     children: [
+                //       Container(
+                //         height: MediaQuery.of(context).size.height * 0.7,
+                //         width: MediaQuery.of(context).size.width * 0.9,
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.circular(20),
+                //           gradient: const LinearGradient(
+                //             colors: [Colors.purple, Colors.blueAccent],
+                //             begin: Alignment.bottomLeft,
+                //             end: Alignment.topRight,
+                //             stops: [0.4, 0.7],
+                //             tileMode: TileMode.repeated,
+                //           ),
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //       Text(
+                //         "Linear Gradient with stops",
+                //         style: GoogleFonts.lato(
+                //           fontSize: 30,
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                // For Radial Gradient without stops and tileMode
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: Column(
+                //     children: [
+                //       Container(
+                //         height: MediaQuery.of(context).size.height * 0.7,
+                //         width: MediaQuery.of(context).size.width * 0.9,
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.circular(20),
+                //           gradient: const RadialGradient(
+                //             colors: [Colors.red, Colors.yellow],
+                //             radius: 0.75,
+                //           ),
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //       Text(
+                //         "Radial Gradient",
+                //         style: GoogleFonts.lato(
+                //           fontSize: 30,
+                //         ),
+                //       ),
+                //       const SizedBox(
+                //         height: 5,
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                // For Radial Gradient with stops and tileMode
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const RadialGradient(
+                            colors: [Colors.red, Colors.yellow],
+                            radius: 0.75,
+                            focal: Alignment(0.7, -0.7),
+                            tileMode: TileMode.clamp,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Radial Gradient with stops",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
