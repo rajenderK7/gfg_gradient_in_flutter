a42838da58e494beac087417f432916ddc03ec65
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2021-12-17 14:59:58 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -47,102 +48,102 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: [
                 // For Linear Gradient without stops and tileMode
-                // Padding(
-                //   padding: const EdgeInsets.all(8.0),
-                //   child: Column(
-                //     children: [
-                //       Container(
-                //         height: MediaQuery.of(context).size.height * 0.7,
-                //         width: MediaQuery.of(context).size.width * 0.9,
-                //         decoration: BoxDecoration(
-                //           borderRadius: BorderRadius.circular(20),
-                //           gradient: const LinearGradient(
-                //             colors: [Colors.purple, Colors.blueAccent],
-                //             begin: Alignment.bottomLeft,
-                //             end: Alignment.topRight,
-                //           ),
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //       Text(
-                //         "Linear Gradient",
-                //         style: GoogleFonts.lato(
-                //           fontSize: 30,
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const LinearGradient(
+                            colors: [Colors.purple, Colors.blueAccent],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Linear Gradient",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
                 // For Linear Gradient with stops and tileMode
-                // Padding(
-                //   padding: const EdgeInsets.all(8.0),
-                //   child: Column(
-                //     children: [
-                //       Container(
-                //         height: MediaQuery.of(context).size.height * 0.7,
-                //         width: MediaQuery.of(context).size.width * 0.9,
-                //         decoration: BoxDecoration(
-                //           borderRadius: BorderRadius.circular(20),
-                //           gradient: const LinearGradient(
-                //             colors: [Colors.purple, Colors.blueAccent],
-                //             begin: Alignment.bottomLeft,
-                //             end: Alignment.topRight,
-                //             stops: [0.4, 0.7],
-                //             tileMode: TileMode.repeated,
-                //           ),
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //       Text(
-                //         "Linear Gradient with stops",
-                //         style: GoogleFonts.lato(
-                //           fontSize: 30,
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const LinearGradient(
+                            colors: [Colors.purple, Colors.blueAccent],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                            stops: [0.4, 0.7],
+                            tileMode: TileMode.repeated,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Linear Gradient with stops",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
                 // For Radial Gradient without stops and tileMode
-                // Padding(
-                //   padding: const EdgeInsets.all(8.0),
-                //   child: Column(
-                //     children: [
-                //       Container(
-                //         height: MediaQuery.of(context).size.height * 0.7,
-                //         width: MediaQuery.of(context).size.width * 0.9,
-                //         decoration: BoxDecoration(
-                //           borderRadius: BorderRadius.circular(20),
-                //           gradient: const RadialGradient(
-                //             colors: [Colors.red, Colors.yellow],
-                //             radius: 0.75,
-                //           ),
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //       Text(
-                //         "Radial Gradient",
-                //         style: GoogleFonts.lato(
-                //           fontSize: 30,
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         height: 5,
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const RadialGradient(
+                            colors: [Colors.red, Colors.yellow],
+                            radius: 0.75,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Radial Gradient",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
                 // For Radial Gradient with stops and tileMode
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -166,6 +167,84 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                       Text(
                         "Radial Gradient with stops",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
+                // Sweep Gradient without start and end angles
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const SweepGradient(
+                            colors: [
+                              Colors.red,
+                              Colors.yellow,
+                              Colors.blue,
+                              Colors.green,
+                              Colors.red,
+                            ],
+                            stops: <double>[0.0, 0.25, 0.5, 0.75, 1.0],
+                            tileMode: TileMode.clamp,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Sweep Gradient",
+                        style: GoogleFonts.lato(
+                          fontSize: 30,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
+                // Sweep Gradient with start and end angles
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: const SweepGradient(
+                            startAngle: math.pi * 0.2,
+                            endAngle: math.pi * 1.7,
+                            colors: [
+                              Colors.red,
+                              Colors.yellow,
+                              Colors.blue,
+                              Colors.green,
+                              Colors.red,
+                            ],
+                            stops: <double>[0.0, 0.25, 0.5, 0.75, 1.0],
+                            tileMode: TileMode.clamp,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "Sweep Gradient with start and stop angles",
                         style: GoogleFonts.lato(
                           fontSize: 30,
                         ),
